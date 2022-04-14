882c3b6cfa42c229d990ae49ff0263986c35d287
Dima <dpolevodin@gmail.com>
2022-04-14 13:51:59 +0500

    added standForm in modal

--- src/components/common/Modal/Modal.tsx
+++ src/components/common/Modal/Modal.tsx
@@ -1,10 +1,21 @@
 import React, { FC } from 'react';
-import { Modal as ModalAntd } from 'antd';
+import { DatePicker, Form, Input, Modal as ModalAntd, Switch } from "antd";
 import { useStore } from 'effector-react'
 import { $isModalDisplayed, setModalUnVisibleEvent } from "src/store/commonWidgets";
 
+const disabledPeriod = () => {
+ const today = Date.now();
+ const maxDate = new Date(today + 7)
+  return maxDate
+}
+
 export const Modal: FC = ({ children}) => {
   const isModalVisible = useStore($isModalDisplayed)
+  const standId = '1-01'
+  const whoIsBusy = 'Иванов Иван'
+
+  const today = Date.now();
+  const maxDate = new Date(today + 7)
 
   const handleOk = () => {
     //TODO: заменить на отправку мутации в БД, чтобы стенд стал занятым
@@ -18,7 +29,7 @@ export const Modal: FC = ({ children}) => {
 
   return (
     <ModalAntd
-      title="Внесите информацию"
+      title="Внести информацию"
       visible={isModalVisible}
       onOk={handleOk}
       onCancel={handleCancel}
@@ -26,7 +37,32 @@ export const Modal: FC = ({ children}) => {
       cancelText='Отмена'
       centered
     >
-      {children}
+      <Form
+        labelCol={{ span: 10 }}
+        layout="horizontal"
+        labelAlign='right'
+      >
+        <Form.Item label="Номер стенда: ">
+          <Input value={standId} disabled />
+        </Form.Item>
+        <Form.Item label="Ветка: " required >
+          <Input />
+        </Form.Item>
+        <Form.Item label="Занял: ">
+          <Input value={whoIsBusy} disabled/>
+        </Form.Item>
+        <Form.Item label="Занять до: " required >
+          <DatePicker
+
+          />
+        </Form.Item>
+        <Form.Item label="Можно проксироваться: " valuePropName="checked">
+          <Switch />
+        </Form.Item>
+        <Form.Item label="Комментарии: ">
+          <Input />
+        </Form.Item>
+      </Form>
     </ModalAntd>
   );
 };
