f394d28967d66dae5c4bdd6d124058cf08ffcefa
Dima <dpolevodin@gmail.com>
2022-06-12 16:22:13 +0500

    changed routing

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
-import { HashRouter } from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom'
 import { ApolloProviderWithClient } from "./components/ApolloProviderWithClient";
 
 ReactDOM.render(
   <ApolloProviderWithClient >
     <React.StrictMode>
-      <HashRouter>
+      <BrowserRouter>
         <App />
-      </HashRouter>
+      </BrowserRouter>
     </React.StrictMode>
   </ApolloProviderWithClient>,
   document.getElementById('root'),
