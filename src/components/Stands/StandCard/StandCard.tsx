1e85fcac830389399d54039edc1b14cf895dd9f4
Dima <dpolevodin@gmail.com>
2022-06-15 19:13:43 +0500

    hide null comments

--- src/components/Stands/StandCard/StandCard.tsx
+++ src/components/Stands/StandCard/StandCard.tsx
@@ -57,7 +57,7 @@ export const StandCard: FC<StandCardProps> = ({
       style={{ width: 300, marginTop: 16, display: 'flex', justifyContent: 'space-between', flexDirection: 'column' }}
       actions={[
         <Tooltip title={formattedComments}>
-          <CommentOutlined style={{ fontSize: '180%'}} hidden={!formattedComments || !isStandBusy}/>
+          <CommentOutlined style={{ fontSize: '180%'}} hidden={formattedComments === "null" || !formattedComments || !isStandBusy }/>
         </Tooltip>,
         <Button type="primary" disabled={isButtonDisabled || isCurrentUserStandsLimitEnabled} onClick={handleClick}>
           {isUserStand ? 'Освободить' : 'Занять'}
