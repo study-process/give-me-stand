949e4aff9dd21be7a1026bef401678135ee71332
Дмитрий Полеводин <digpolevodin@sberbank.ru>
2022-08-21 17:48:33 +0500

    added loader to content stands page

--- src/constants.ts
+++ src/constants.ts
@@ -8,3 +8,5 @@ export enum NavigationPageTypesEnum {
 export const AUTHORIZED_URL = 'https://give-me-stand-server.herokuapp.com/login'
 export const LOCAL_STORAGE_USER = 'currentUser'
 export const ROOT_URL = ''
+
+export const ERROR_DUPLICATE_STAND = 'Error: Uniqueness violation. duplicate key value violates unique constraint "stands_pkey"'
--- src/pages/AdminPage/pages/ContentStandsPage/ContentStandsPage.tsx
+++ src/pages/AdminPage/pages/ContentStandsPage/ContentStandsPage.tsx
@@ -1,8 +1,8 @@
-import React, { FC } from "react";
+import React, { FC, useEffect } from "react";
 import { useStore } from 'effector-react'
 import { useMutation } from "@apollo/client";
 
-import { List, Button, Form, Input, Alert } from "antd";
+import { List, Button, Form, Input, Alert, Spin } from "antd";
 import {CloudServerOutlined} from '@ant-design/icons'
 import './styles.css'
 import { $stands, getStandsEvent } from "../../../../store/stands";
@@ -10,19 +10,23 @@ import { StandCardProps } from "../../../../components/Stands/StandCard/interfac
 import { CREATE_STAND, GET_ALL_STANDS } from "../../../../gql";
 import { $displayErrorWarning, setErrorWarningEvent } from "../../../../store";
 import { adminLoginMessageTypesEnum } from "../../../LoginPage/constants";
+import { $serverResponseIsLoading, serServerResponseLoadingEvent } from "../../../../store/serverResponse";
+import { ERROR_DUPLICATE_STAND } from "../../../../constants";
 
 export const ContentStandsPage: FC = () => {
   const stands: StandCardProps[] = useStore($stands)
   const errorWarningMessage = useStore($displayErrorWarning)
+  const serverResponseIsLoading = useStore($serverResponseIsLoading)
 
   getStandsEvent('/stands')
 
   const [form] = Form.useForm();
 
-  const [CreateStand, {loading, error}] = useMutation(CREATE_STAND)
+  const [CreateStand, {loading, error, data}] = useMutation(CREATE_STAND)
 
   const handleSubmit = (values: { standNumber: string, team: string }) => {
     if (values.standNumber && values.team) {
+      serServerResponseLoadingEvent(true)
       CreateStand({
         variables: {
           id: values.standNumber,
@@ -34,59 +38,67 @@ export const ContentStandsPage: FC = () => {
         awaitRefetchQueries: true,
       })
     }
-    if (!error) {
-      alert(`Добавлен стенд ${values.standNumber}`)
-    }
   };
+
+  useEffect(() => {
+    if (data || error) {
+      serServerResponseLoadingEvent(false)
+    }
+    if (error && String(error) === ERROR_DUPLICATE_STAND) {
+      alert("Ошибка: Стенд с таким номером уже существует!")
+    }
+  }, [error, data])
 
   const handleFinishFailed = () => {
     setErrorWarningEvent()
   }
 
   return <div className="admin-page__users-container">
-    {errorWarningMessage && (
-      <Alert message={adminLoginMessageTypesEnum.incorrect} type="error" style={{ marginBottom: "2rem" }}/>
-    )}
-    <Form
-      layout='inline'
-      form={form}
-      style={{marginBottom: '2rem'}}
-      onFinish={handleSubmit}
-      onFinishFailed={handleFinishFailed}
-    >
-      <Form.Item label="Номер стенда" name="standNumber" rules={[
-        {
-          required: true,
-          message: `Номер стенда не заполнен`,
-        },
-      ]}>
-        <Input placeholder="В формате X-XX"/>
-      </Form.Item>
-      <Form.Item label="Команда" name="team" rules={[
-        {
-          required: true,
-          message: `Команда не заполнена`,
-        },
-      ]}>
-        <Input placeholder="Номер команды" />
-      </Form.Item>
-      <Form.Item >
-        <Button type="primary" htmlType="submit" >Добавить стенд</Button>
-      </Form.Item>
-    </Form>
-    <List
-      itemLayout="horizontal"
-      dataSource={stands}
-      renderItem={item => (
-        <List.Item>
-          <List.Item.Meta
-            avatar={<CloudServerOutlined />}
-            title={item?.id}
-            description={`Команда: ${item?.team}`}
-          />
-          <Button type="primary" danger>Удалить</Button>
-        </List.Item>
+    <Spin spinning={serverResponseIsLoading} delay={500}>
+      {errorWarningMessage && (
+        <Alert message={adminLoginMessageTypesEnum.incorrect} type="error" style={{ marginBottom: "2rem" }}/>
       )}
-    />
+      <Form
+        layout='inline'
+        form={form}
+        style={{marginBottom: '2rem'}}
+        onFinish={handleSubmit}
+        onFinishFailed={handleFinishFailed}
+      >
+        <Form.Item label="Номер стенда" name="standNumber" rules={[
+          {
+            required: true,
+            message: `Номер стенда не заполнен`,
+          },
+        ]}>
+          <Input placeholder="В формате X-XX"/>
+        </Form.Item>
+        <Form.Item label="Команда" name="team" rules={[
+          {
+            required: true,
+            message: `Команда не заполнена`,
+          },
+        ]}>
+          <Input placeholder="Номер команды" />
+        </Form.Item>
+        <Form.Item >
+          <Button type="primary" htmlType="submit" >Добавить стенд</Button>
+        </Form.Item>
+      </Form>
+      <List
+        itemLayout="horizontal"
+        dataSource={stands}
+        renderItem={item => (
+          <List.Item>
+            <List.Item.Meta
+              avatar={<CloudServerOutlined />}
+              title={item?.id}
+              description={`Команда: ${item?.team}`}
+            />
+            <Button type="primary" danger>Удалить</Button>
+          </List.Item>
+        )}
+      />
+    </Spin>
   </div>
 }
